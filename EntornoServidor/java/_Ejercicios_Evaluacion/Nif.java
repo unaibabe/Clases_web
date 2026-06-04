0f64fefa4901be5c73852b5d75add2a8ac0df03f
unaii <65370390+unaibabe@users.noreply.github.com>
2026-06-04 08:58:22 +0200

    Add files via upload

---
+++ EntornoServidor/java/_Ejercicios_Evaluacion/Nif.java
@@ -0,0 +1,67 @@
+package java;
+
+import static java.lang.Integer.parseInt;
+
+/**
+ * Clase para operaciones con dnis
+ * @author unai
+ * @version 03/06/2026
+ */
+public class Nif {
+    private String nif;
+    private String LETRAS= "TRWAGMYFPDXBNJZSQVHLCKE";
+
+    /**
+     * Methodo constructor
+     * @param pNif
+     */
+    public Nif(String pNif){
+        this.nif = pNif;
+    }
+
+    /**
+     * Retorna el nif
+     * @return nif
+     */
+    public String getNif(){
+        return this.nif;
+    }
+
+    /**
+     * Metodo para setear un nif
+     * @param pNif
+     */
+    public void setNif(String pNif){
+        this.nif = pNif;
+    }
+
+    /**
+     * Metodo para obtener la letra del dni introducido
+     * @return la letra del dni que tiene la clase
+     */
+    public char getLetra(){
+        char letra = this.nif.charAt(this.nif.length()-1);
+        return letra;
+    }
+
+    /**
+     * Metodo para calcular que letra deberia tener el dni
+     * @return la letra que deberia tener
+     */
+    public char calcularLetra(){
+        String numerosDni = this.nif.substring(0,8);
+        //System.out.println(numerosDni);
+        int numeroParaCalculoLetra = parseInt(numerosDni)%23;
+        char letraCalculada = LETRAS.charAt(numeroParaCalculoLetra);
+        return letraCalculada;
+    }
+
+    /**
+     * Metodo para comparar si la letra que tiene el dni y la que deberia tener coinciden
+     * @return booleano true si es verdadero y false si no coincide
+     */
+    public boolean esValido(){
+        return (getLetra()==calcularLetra());
+    }
+
+}
---
+++ EntornoServidor/java/_Ejercicios_Evaluacion/Pruebas.java
@@ -0,0 +1,11 @@
+package java;
+
+public class Pruebas {
+    public static void main(String[] args){
+        Nif nid_1 = new Nif("87654321X");
+
+//        System.out.println(nid_1.getLetra());
+        System.out.println(nid_1.calcularLetra());
+        System.out.println(nid_1.esValido());
+    }
+}
